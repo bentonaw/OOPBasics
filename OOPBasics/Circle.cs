27e72b66454b45a275746afce7bdc0cf035be797
Huan <huanyang.ooi@gmail.com>
2023-10-13 00:20:29 +0200

    Add project files.

---
+++ OOPBasics/Circle.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPBasics
+{
+    internal class Circle
+    {
+        private int _radius { get; set; }
+
+        internal Circle (int radius)
+        {
+            _radius = radius;
+        }
+
+        public double GetArea()
+        {
+            return ((_radius * _radius) * Math.PI);
+        }
+    }
+}
---
+++ OOPBasics/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace OOPBasics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Circle circle1 = new Circle(5);
+            Circle circle2 = new Circle(6);
+
+            Console.WriteLine("Area for the first circle with radius 5 is " + circle1.GetArea());
+            Console.WriteLine("Area for the first circle with radius 6 is " + circle2.GetArea());
+        }
+    }
+}
