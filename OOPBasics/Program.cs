9fff2509ccbdb71153e559e2555877fa7a20cea1
Huan <huanyang.ooi@gmail.com>
2023-10-20 18:07:57 +0200

    done

--- OOPBasics/Program.cs
+++ OOPBasics/Program.cs
@@ -15,62 +15,15 @@
             Console.WriteLine($"Circumference: {circle2.GetCircumference()} cm");
             Console.WriteLine($"Volume: {circle2.GetVolume()} cm³");
 
-            Console.WriteLine("\nTo calculate the area of a triangle we will need to start with which information you have on the triangle: " +
-                "\n1. Length of two side (a+b)" +
-                //"\n2. Lengths of all sides (a+b+c)" +
-                //"\n3. Lengths of two sides (a+b) and the angle between them(θ)" +
-                //"\n4. Quit");
-            //"\n4. Two angles(θ1+θ2) and the length between them(a)"); // Skippar detta för tillfället
-
+            Console.WriteLine("\nTo calculate the area of a triangle I will need the length of the base and height (length of the longest line perpendicular to the base)");
             Triangle triangle1 = new Triangle();
 
-            Console.Write("\nPlease select an option between 1-4: ");
-            int choice = int.Parse(Console.ReadLine());
-            switch (choice)
-            {
-                case 1:
-                    Console.Write("Length of a: ");
-                    triangle1.A = double.Parse(Console.ReadLine());
-                    Console.Write("Length of b: ");
-                    triangle1.B = double.Parse(Console.ReadLine());
+            Console.Write("Length of a: ");
+            triangle1.A = double.Parse(Console.ReadLine());
+            Console.Write("Length of b: ");
+            triangle1.B = double.Parse(Console.ReadLine());
 
-                    Console.WriteLine(triangle1.GetArea2sides());
-                    
-                    break;
-                //case 2:
-                //    Console.Write("Length of a: ");
-                //    triangle1.A = double.Parse(Console.ReadLine());
-                //    Console.Write("Length of b: ");
-                //    triangle1.B = double.Parse(Console.ReadLine());
-                //    Console.Write("Length of c: ");
-                //    triangle1.C = double.Parse(Console.ReadLine());
-
-                //    Console.WriteLine(triangle1.GetArea3sides());
-                //    break;
-                //case 3:
-                //    Console.Write("Length of a: ");
-                //    triangle1.A = double.Parse(Console.ReadLine());
-                //    Console.Write("Length of b: ");
-                //    triangle1.B = double.Parse(Console.ReadLine());
-                //    Console.Write("Angle between a and b: ");
-                //    triangle1.Angle1 = double.Parse(Console.ReadLine());
-
-                //    Console.WriteLine(triangle1.GetArea2sides1Angle());
-                //    break;
-                //case 4:
-                //    Console.Write("First angle (Y): ");
-                //    triangle1.θ1 = double.Parse(Console.ReadLine());
-                //    Console.Write("Second Angle (β): ");
-                //    triangle1.θ2 = double.Parse(Console.ReadLine());
-                //    Console.Write("Length between the angles: ");
-                //    triangle1.a = double.Parse(Console.ReadLine());
-                //    break;
-                default:
-                    break;
-            }
-
-
-
+            Console.WriteLine("The area of the triangle is " + triangle1.GetArea2sides() + " units");
 
         }
     }
--- OOPBasics/Triangle.cs
+++ OOPBasics/Triangle.cs
@@ -10,25 +10,10 @@ namespace OOPBasics
     {
         public double A { get; set; }
         public double B { get; set; }
-        public double C { get; set; }
-        public double Angle1 { get; set; }
-
-        //public double Angle2 { get; set; }
- 
-        public double s { get; set; }
 
         public double GetArea2sides()
         {
             return (A * B) / 2.0;
-        }
-        public double GetArea3sides()
-        {
-            s = (A + B + C) / 2.0;
-            return Math.Sqrt(s * (s - A) * (s - B) * (s - C));
-        }
-        public double GetArea2sides1Angle()
-        {
-            return (A * B * Math.Sin(Angle1)) / 2.0;
         }
     }
 }
