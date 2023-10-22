c2f6f99be33fec34db3a403d98b6b3cbd0c3b7fc
Huan <huanyang.ooi@gmail.com>
2023-10-22 18:37:43 +0200

    done

--- OOPBasics/Program.cs
+++ OOPBasics/Program.cs
@@ -4,8 +4,8 @@
     {
         static void Main(string[] args)
         {
-            Circle circle1 = new Circle(5); // första instansieringen av cirkel klassen med radie 5
-            Circle circle2 = new Circle(6); // andra instansieringen av cirkel klassen med radie 6
+            Circle circle1 = new Circle(5); // first object of circle class with radius 5
+            Circle circle2 = new Circle(6); // second object of circle class with radius 6
 
             Console.WriteLine($"Area for the first circle with radius 5 is: {circle1.GetArea()} cm²");
             Console.WriteLine($"Circumference: {circle1.GetCircumference()} cm");
@@ -15,7 +15,7 @@
             Console.WriteLine($"Circumference: {circle2.GetCircumference()} cm");
             Console.WriteLine($"Volume: {circle2.GetVolume()} cm³");
 
-            Triangle triangle1 = new Triangle(5, 6); // instansiering av triangel klassen för uträkning av arean när man vet längden på två sidor samt har en 90 gradig vinkel mellan dem.
+            Triangle triangle1 = new Triangle(5, 6); // instance of triangle class to get area where two sides are known and the angle between them is 90 degrees.
 
             Console.WriteLine($"\nThe area of a triangle with the base length 5 and height 6 is: {triangle1.GetArea2sides()} units");
 
